61bb3e8b3d52fb8de3a5fb1f25c268b4ef51b894
YasinZhong <1224175213@qq.com>
2017-08-03 12:11:58 +0800

    好的更改

---
+++ cssjs/baidu.js
@@ -0,0 +1,8 @@
+﻿
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?77d1c881940ce16821d72caadfa9fe11";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
