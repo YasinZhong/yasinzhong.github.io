e8021436e80be60f49efaadb539a9d724e302a55
yasinzhong <1224175213@qq.com>
2018-03-01 15:55:11 +0800

    2018/03/01

--- cssjs/a.js
+++ cssjs/a.js
@@ -7,6 +7,7 @@
 		VarDivProb.style="width:1cm;";
 		VarBody.appendChild(VarDivProb);
 		//以上创建一个探针，以供下面计算屏幕
+		
 
 		var windoww=window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;
 		var windowh=window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
@@ -16,12 +17,15 @@
 		
 		function FGoodWidth(id)
 		{
-			if( windoww<windowh )document.getElementById(id).style.width="90%";
-			//只要是竖屏状态。
-			if( windoww>windowh && windowwcm<=38)document.getElementById(id).style.width=windoww*0.75+"px";
-			//cm小于 38 厘米，通常是横屏的移动设备或低分电脑
-			if( windoww>windowh && windowwcm>38)document.getElementById(id).style.width=windoww*0.5+"px";
+			if( windoww<windowh )
+			{document.getElementById(id).style.width="95%";}//只要是竖屏状态。
+			
+			if( windoww>windowh && windowwcm<=38)
+			{document.getElementById(id).style.width=windoww*0.75+"px";}//cm小于 38 厘米，通常是横屏的移动设备或低分电脑
+			
+			if( windoww>windowh && windowwcm>38)
+			{document.getElementById(id).style.width=windoww*0.5+"px";}
 			//调整展示样式，如果是移动设备【宽度大于高度是横屏，宽度小于 38 厘米，通常是移动设备，或者低分辨率的电脑
 		}
-		FGoodWidth("div-content");
+		FGoodWidth("div-content"); 
 		FGoodWidth("div-header");
